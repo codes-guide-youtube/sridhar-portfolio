923efb98d0e3a63919d0475f31b746110046042c
Basic-Funda-Developer <100834471+codes-guide-youtube@users.noreply.github.com>
2022-12-30 14:26:42 +0530

    v1

---
+++ components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Navbar = () => (
+    <nav>
+        <div>
+            <span className="nav-head">Codes Guide</span>
+            <ul className="flex ">
+                <li></li>
+            </ul>
+        </div>
+    </nav>
+)
+
+export default Navbar
