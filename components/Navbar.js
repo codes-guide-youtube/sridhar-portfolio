9ab0497cf6ba75f12ab0f562986682a591087de2
Basic-Funda-Developer <100834471+codes-guide-youtube@users.noreply.github.com>
2022-12-30 14:26:51 +0530

    v1

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Navbar = () => (
     <nav>
-        <div>
-            <span className="nav-head">Codes Guide</span>
-            <ul className="flex ">
-                <li></li>
+        <div className="align-middle bg-cyan-700 text-white">
+            
+            <span className="nav-head flex space-x-3"><Image src="/favicon.png" width="32" height="32" /></span>
+            <ul className="font-semibold flex justify-center space-x-3">
+                <li className="nav-item"><Link href="/">Home</Link></li>
+                <li><Link href="/about">About</Link></li>
             </ul>
         </div>
     </nav>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import NextNProgress from 'nextjs-progressbar';
+import Navbar from '../components/Navbar';
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div> 
+    <Navbar/>
+    <NextNProgress color="#00C1FF" options={{showSpinner: false}}/>
+    <Component {...pageProps} />
+  </div>
 }
